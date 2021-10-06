226b2b76bf7931e6c24bda020fc97f9d829a19c1
YellowH2312 <hellou8363@gmail.com>
2021-10-06 20:24:40 +0900

    문자열을 정수로 바꾸기
    Level 1

---
+++ Q2.java
@@ -0,0 +1,14 @@
+public class Q2 {
+    public static void main(String[] args) {
+        ChangeNum changeNum = new ChangeNum();
+        String str = "1234";
+        System.out.println(changeNum.ChangeNum(str));
+    }
+}
+class ChangeNum{
+    public int ChangeNum(String s){
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
